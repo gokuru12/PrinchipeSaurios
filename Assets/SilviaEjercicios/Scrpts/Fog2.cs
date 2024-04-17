b48d58d54a8ede9bf1b5275b2d8bc2e2a89f4018
Sofsil-28 <sofimermaid8@gmail.com>
2024-04-16 19:33:09 -0500

    nieblaaa
    a

---
+++ Assets/SilviaEjercicios/Scrpts/Fog2.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+public class Fog2 : ScriptableRenderPass
+{
+    private RTHandle postProcessTempTexture;
+
+    private Material renderingMaterial;
+    public Fog2(Material renderingMaterial)
+    {
+    
+    this.renderingMaterial = renderingMaterial; 
+
+    }
+    // This method is called before executing the render pass.
+    // It can be used to configure render targets and their clear state. Also to create temporary render target textures.
+    // When empty this render pass will render to the active camera render target.
+    // You should never call CommandBuffer.SetRenderTarget. Instead call <c>ConfigureTarget</c> and <c>ConfigureClear</c>.
+    // The render pipeline will ensure target setup and clearing happens in a performant manner.
+    public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
+    {
+        RenderTextureDescriptor descriptor  = renderingData.cameraData.cameraTargetDescriptor;
+        RenderingUtils.ReAllocateIfNeeded(ref postProcessTempTexture,descriptor );
+
+    }
+
+    // Here you can implement the rendering logic.
+    // Use <c>ScriptableRenderContext</c> to issue drawing commands or execute command buffers
+    // https://docs.unity3d.com/ScriptReference/Rendering.ScriptableRenderContext.html
+    // You don't have to call ScriptableRenderContext.submit, the render pipeline will call it at specific points in the pipeline.
+    public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+    {
+        CommandBuffer cmd = CommandBufferPool.Get("Valorant Smokes");
+        RTHandle screenTexture = renderingData.cameraData.renderer.cameraColorTargetHandle; 
+        cmd.Blit( screenTexture, postProcessTempTexture);
+    }
+
+    // Cleanup any allocated resources that were created during the execution of this render pass.
+    public override void OnCameraCleanup(CommandBuffer cmd)
+    {
+    }
+}
