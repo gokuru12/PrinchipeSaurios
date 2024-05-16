23a97d101a2a950c5c1482352be3f9246536c1c9
gokuru12 <71029151+gokuru12@users.noreply.github.com>
2024-05-16 18:26:37 -0500

    Cosas que hizo el profe

---
+++ Assets/William_Assets/Explicación profe/Profeexplicandoalgo.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Profeexplicandoalgo : MonoBehaviour
+{
+    [SerializeField] private GameObject[] effectPrefabs;
+
+
+public void IsolatedAndPlayEffect(int effect)
+    {
+
+    }
+}
